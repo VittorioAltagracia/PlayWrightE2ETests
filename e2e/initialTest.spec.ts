8236170985ac5eaa0f5eb2d5eadbcc06c8c6438f
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-07-29 20:50:46 -0500

    first UI tests

--- e2e/initialTest.spec.ts
+++ e2e/initialTest.spec.ts
@@ -7,6 +7,26 @@ test.beforeEach(async ({ page }) => {
   await page.goto(baseUrl);
 });
 
-test("has title", async ({ page }) => {
-  await expect(page).toHaveTitle(pageTitle);
+test.describe("UI tests", () => {
+  test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle(pageTitle);
+  });
+
+  test("Confirm main header text", async ({ page }) => {
+    await expect(page.getByText("Universal Source")).toBeVisible();
+  });
+
+  test("Confirm toast message is present", async ({ page }) => {
+    await page.getByRole("contentinfo").locator("div").filter({
+      hasText:
+        "Please noteThis application is still in development and may contain fake data an",
+    });
+  });
+
+  test("Verify main header is present", async ({ page }) => {
+    await page.getByRole("heading").locator("h2").filter({
+      hasText:
+        "New in the US? Find quick answers to the most important questions.",
+    });
+  });
 });
